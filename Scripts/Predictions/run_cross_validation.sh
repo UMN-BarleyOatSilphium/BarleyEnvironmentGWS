e037d6b2df885799c6cb155fe52c613db1b5729e
neyhartj <neyha001@umn.edu>
2019-07-15 15:27:00 -0500

    edits to bash

--- Scripts/Predictions/run_cross_validation.sh
+++ Scripts/Predictions/run_cross_validation.sh
@@ -2,12 +2,12 @@
 
 # #PBS -l walltime=48:00:00,mem=62gb,nodes=1:ppn=8
 # #PBS -l walltime=72:00:00,mem=62gb,nodes=1:ppn=16
-#PBS -l walltime=24:00:00,mem=64gb,nodes=1:ppn=8
+#PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=16
 # #PBS -N cross-validation-cv1
 # #PBS -N cross-validation-cv2
 # #PBS -N cross-validation-cv00
-# #PBS -N parent-offspring-validation
-#PBS -N prediction-validation-sample
+#PBS -N parent-offspring-validation
+# #PBS -N prediction-validation-sample
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -26,8 +26,8 @@ module load R/3.5.0
 # Rscript cross_validation_cv00.R
 
 ## Parent-offspring validation
-# Rscript parent_offspring_validation.R
+Rscript parent_offspring_validation.R
 
 ## Sample script
-Rscript prediction_validation_sample.R
+# Rscript prediction_validation_sample.R
 
