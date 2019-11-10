bdc202a47685868b18f05bccc776d70c5d401777
neyhartj <neyha001@umn.edu>
2019-11-10 12:51:45 -0600

    edits to bash

--- Scripts/Predictions/run_cross_validation.sh
+++ Scripts/Predictions/run_cross_validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=12:00:00,mem=48gb,nodes=1:ppn=8
+#PBS -l walltime=24:00:00,mem=96gb,nodes=1:ppn=1
 # #PBS -l walltime=36:00:00,mem=62gb,nodes=1:ppn=8
 # #PBS -l walltime=02:00:00,mem=62gb,nodes=1:ppn=16
 # #PBS -N cross-validation-cv1
