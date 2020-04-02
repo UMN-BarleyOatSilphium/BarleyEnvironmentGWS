dacbae49d3f6b5ba88ff8b004358550cec151564
neyhartj <neyha001@umn.edu>
2020-04-02 10:51:37 -0500

    Edits to bash

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=12:00:00,mem=62gb,nodes=1:ppn=8
+#PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=8
 # #PBS -N loeo_predictions
 # #PBS -N loyo_predictions
 #PBS -N lolo_predictions
