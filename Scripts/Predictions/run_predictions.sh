b36e2d148e2fc77ed4d3fb3b177786a31953c463
neyhartj <neyha001@umn.edu>
2020-09-25 12:02:12 -0500

    more edits to the bash script

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -l walltime=48:00:00,mem=62gb,nodes=1:ppn=8
-# #PBS -l walltime=04:00:00,mem=62gb,nodes=1:ppn=12
-#PBS -N loeo_predictions_fr
-# #PBS -N lolo_predictions_fr
+# #PBS -l walltime=48:00:00,mem=62gb,nodes=1:ppn=8
+#PBS -l walltime=08:00:00,mem=62gb,nodes=1:ppn=12
+# #PBS -N loeo_predictions_fr
+#PBS -N lolo_predictions_fr
 # #PBS -N cv_7525_predictions_fr
 # #PBS -N lolo_predictions_longterm_fr
 #PBS -M neyha001@umn.edu
@@ -18,10 +18,10 @@ module load R/3.5.2_mkl
 ## Factorial regression 
 
 # Leave-one-environment-out
-Rscript environment_loeo_predictions2.R
+# Rscript environment_loeo_predictions2.R
 
 # # Leave-one-location-out
-# Rscript environment_lolo_predictions2.R
+Rscript environment_lolo_predictions2.R
 
 
 ## 75-25 cross-validation
